0f898ea165083f194010351957061fda4fd86442
MatteoRuffa <cortese_17@protonmail.com>
2024-06-06 17:53:38 +0200

    hopefully ended

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -21,13 +21,16 @@
             <a class="nav-link active{{Route::currentRouteName() == 'admin.dashboard' ? 'active' : ''}}" 
             href="{{route('admin.dashboard')}}" aria-current="page" >Dasboard</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link {Route::currentRouteName() == 'admin.posts.index' ? 'active' : ''}}" 
+            href="{{route('admin.projects.index')}}">Project</a>
+          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Carpet
             </a>
             <ul class="dropdown-menu dropdown-menu-dark">
-              <li><a class="dropdown-item {Route::currentRouteName() == 'admin.posts.index' ? 'active' : ''}}" 
-                href="{{route('admin.projects.index')}}">Project</a></li>
+            <li><a class="dropdown-item" href="#">Action</a></li>
               <li><a class="dropdown-item" href="#">Another action</a></li>
               <li>
                 <!-- <hr class="dropdown-divider"> -->
